9695a2574d83a93c51908a013c487606398f50ca
caiopgaspar <c-gaspar@hotmail.com>
2025-01-15 20:54:35 -0300

    demais desafios

--- desafio-target/Program.cs
+++ desafio-target/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace desafio_target
 {
@@ -72,16 +74,55 @@ namespace desafio_target
         // desafio 3
         static void Desafio3()
         {
+            var faturamento = new List<double> { 221.5, 0, 345.7, 0, 150.3 };
+            var valores = faturamento.Where(x => x > 0).ToList();
+            double menor = valores.Min();
+            double maior = valores.Max();
+            double media = valores.Average();
+            int diasAcimaDaMedia = valores.Count(v => v > media);
+
+
+            Console.WriteLine($"Menor valor ocorrido: {menor}");
+
+            Console.WriteLine($"Maior valor ocorido: {maior}");
+
+            Console.WriteLine($"Número de dias do mês acima  da média mensal: {diasAcimaDaMedia}");
         }
 
         // desafio 4
         static void Desafio4()
-        {
+        {            
+            double faturamentoSP = 67836.43;
+            double faturamentoRJ = 36678.66;
+            double faturamentoMG = 29229.88;
+            double faturamentoES = 27165.48;
+            double faturamentoOutros =  19849.53;
+           
+            double total = faturamentoSP + faturamentoRJ + faturamentoMG + faturamentoES + faturamentoOutros;
+
+            Console.WriteLine($"SP: {((faturamentoSP / total) * 100):F2}%");
+            Console.WriteLine($"RJ: {((faturamentoRJ / total) * 100):F2}%");
+            Console.WriteLine($"MG: {((faturamentoMG / total) * 100):F2}%");
+            Console.WriteLine($"ES: {((faturamentoES / total) * 100):F2}%");
+            Console.WriteLine($"Outros: {((faturamentoOutros / total ) * 100):F2}%");
         }
 
-        // desafio 5
+            // desafio 5
         static void Desafio5()
         {
+            Console.Write(" Digite uma string:  ");
+
+            string stringDigitada = Console.ReadLine();
+
+            string stringInvertida = "";
+
+
+            for (int i = stringDigitada.Length - 1; i >= 0; i--)
+            {
+                stringInvertida += stringDigitada[i];
+            }
+
+            Console.WriteLine( $"String invertda : {stringInvertida}");
         }
     }
 }
