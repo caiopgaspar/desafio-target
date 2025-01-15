86c962284bfeb6b7284701f9cd6888e59599abdd
caiopgaspar <c-gaspar@hotmail.com>
2025-01-15 20:39:26 -0300

    desafios 1 e 2

---
+++ desafio-target/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace desafio_target
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desafio 1:");
+            Desafio1();
+            Console.WriteLine();
+
+            Console.WriteLine("Desafio 2:");
+            Desafio2();
+            Console.WriteLine();
+
+            Console.WriteLine("Desafio 3:");
+            Desafio3();
+            Console.WriteLine();
+
+            Console.WriteLine("Desafio 4:");
+            Desafio4();
+            Console.WriteLine();
+
+            Console.WriteLine("Desafio 5:");
+            Desafio5();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        // desafio 1
+        static void Desafio1()
+        {
+            int indice = 13, soma = 0, k = 0;
+
+            while (k < indice)
+            {
+                k++;
+                soma = soma + k;
+            }
+
+            Console.WriteLine(soma);            
+        }
+
+        // desafio 2
+        static void Desafio2()
+        {
+            Console.Write("Digite um número a seguir: ");
+
+            int numero = int.Parse(Console.ReadLine());
+            int a = 0, b = 1;
+            int proximo;
+
+            while (b < numero)
+            {
+                proximo = a + b;
+                a = b;
+                b = proximo;
+            }
+
+            if (b == numero || numero == 0)
+            {
+                Console.WriteLine($"O número {numero} pertence à sequência de Fibonaci.");
+            }
+            else
+            {
+                Console.WriteLine($"O numero {numero} não pertence à sequência de Fibonacci.");
+            }
+        }
+
+        // desafio 3
+        static void Desafio3()
+        {
+        }
+
+        // desafio 4
+        static void Desafio4()
+        {
+        }
+
+        // desafio 5
+        static void Desafio5()
+        {
+        }
+    }
+}
---
+++ desafio-target/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("desafio-target")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("desafio-target")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("4f98de69-ec84-4fc0-a95e-aba11ff6c12f")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
